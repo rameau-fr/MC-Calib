3beb89d16d04cd4547a46e7b415142a8aca10387
Alex Bailo <alexandr.baylo@gmail.com>
2024-05-27 23:09:36 +0200

    Applying isort and black

--- python_utils/display_object_calib.py
+++ python_utils/display_object_calib.py
@@ -1,25 +1,28 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import cv2
 import pdb
 
-from mpl_toolkits.mplot3d import proj3d
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D, proj3d
+
 
 def axisEqual3D(ax):
-    extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
-    sz = extents[:,1] - extents[:,0]
+    extents = np.array([getattr(ax, "get_{}lim".format(dim))() for dim in "xyz"])
+    sz = extents[:, 1] - extents[:, 0]
     centers = np.mean(extents, axis=1)
     maxsize = max(abs(sz))
-    r = maxsize/2
-    for ctr, dim in zip(centers, 'xyz'):
-        getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
+    r = maxsize / 2
+    for ctr, dim in zip(centers, "xyz"):
+        getattr(ax, "set_{}lim".format(dim))(ctr - r, ctr + r)
+
 
 #!!!! This display code assumes a single objects remain at the end the calibration process
 # 1. path to object calibration results
-#path_root = "/home/francois/Documents/CppProject/Stereo_Calibration/Images_Synth_4cam_8boards_4groups_NonOverlap/"
+# path_root = "/home/francois/Documents/CppProject/Stereo_Calibration/Images_Synth_4cam_8boards_4groups_NonOverlap/"
 path_root = "/disk/francois/Projets/Calibration_toolbox/Data/Real_Images/stereo_cube/"
-path_root = "/disk/francois/Projets/Dynamic_Nerf_Dataset/0-Calibration/data/Sequence_Calib_RGB/"
+path_root = (
+    "/disk/francois/Projets/Dynamic_Nerf_Dataset/0-Calibration/data/Sequence_Calib_RGB/"
+)
 path_object_results = path_root + "calibrated_objects_data.yml"
 
 marker_size = 1
@@ -29,30 +32,34 @@ fs = cv2.FileStorage(path_object_results, cv2.FILE_STORAGE_READ)
 obj_id = "object_" + str(0)
 obj_mat = fs.getNode(obj_id).getNode("points").mat()
 
-#2. plot
+# 2. plot
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_proj_type('ortho')
-ax.set_title('3D calibrated object')
-#ax.scatter(obj_mat[0,:], obj_mat[1,:], obj_mat[2,:], c=(1.0,0.0,0.0))
+ax = fig.gca(projection="3d")
+ax.set_proj_type("ortho")
+ax.set_title("3D calibrated object")
+# ax.scatter(obj_mat[0,:], obj_mat[1,:], obj_mat[2,:], c=(1.0,0.0,0.0))
 
 
-board_idx = np.unique(obj_mat[3,:]).astype(int)
+board_idx = np.unique(obj_mat[3, :]).astype(int)
 for b_idx in board_idx:
-    pts_3D = obj_mat[0:3,obj_mat[3,:]==b_idx] #gather all the points from the same board
-    color = np.random.rand(3) # random color
-    ax.scatter(pts_3D[0,:], pts_3D[1,:], pts_3D[2,:], c=(color[0],color[1],color[2]))
+    pts_3D = obj_mat[
+        0:3, obj_mat[3, :] == b_idx
+    ]  # gather all the points from the same board
+    color = np.random.rand(3)  # random color
+    ax.scatter(
+        pts_3D[0, :], pts_3D[1, :], pts_3D[2, :], c=(color[0], color[1], color[2])
+    )
 
-#ax.axis('equal')
-#ax.set_aspect('equal', 'box')
-#fig.tight_layout()
+# ax.axis('equal')
+# ax.set_aspect('equal', 'box')
+# fig.tight_layout()
 ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
 ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
 ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
 axisEqual3D(ax)
-ax.set_xlabel('x (cm)',fontsize=20)
-ax.set_ylabel('y (cm)',fontsize=20)
-ax.set_zlabel('z (cm)',fontsize=20)
+ax.set_xlabel("x (cm)", fontsize=20)
+ax.set_ylabel("y (cm)", fontsize=20)
+ax.set_zlabel("z (cm)", fontsize=20)
 plt.show()
 
 # 2. Display the cameras
--- python_utils/error_statistic.py
+++ python_utils/error_statistic.py
@@ -1,20 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
 import pdb
 import random
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 #!!!! This display code assumes a single camera group remain at the end the calibration process
 # 1. path to object calibration results
 path_reprojection_error = "/home/francois/Documents/CppProject/Stereo_Calibration/Images_NonOver6Cam/reprojection_error_data.yml"
 Nb_Camera = 1000
 
-#Generate one color randomly per camera
+# Generate one color randomly per camera
 camera_color = []
-for i in range(0,Nb_Camera):
-    c=(random.uniform(0, 1),random.uniform(0, 1),random.uniform(0, 1))
+for i in range(0, Nb_Camera):
+    c = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
     camera_color.append(c)
-
 
 
 # 2. Open the file
@@ -26,38 +26,42 @@ frame_list = fs.getNode(camera_group_id).getNode("frame_list").mat()
 list_mean_error = []
 list_color = []
 list_mean_error_frame = []
-for i in range(0,frame_list.shape[0]):
+for i in range(0, frame_list.shape[0]):
     frame_id = "frame_" + str(frame_list[i][0])
-    camera_list = fs.getNode(camera_group_id).getNode(frame_id).getNode("camera_list").mat()
+    camera_list = (
+        fs.getNode(camera_group_id).getNode(frame_id).getNode("camera_list").mat()
+    )
     errors_current_frame = []
-    
-    for j in range(0,camera_list.shape[0]):
+
+    for j in range(0, camera_list.shape[0]):
         camera_id = "camera_" + str(camera_list[j][0])
-        error_pts = fs.getNode(camera_group_id).getNode(frame_id).getNode(camera_id).getNode("error_list").mat()
+        error_pts = (
+            fs.getNode(camera_group_id)
+            .getNode(frame_id)
+            .getNode(camera_id)
+            .getNode("error_list")
+            .mat()
+        )
         mean_err = np.mean(error_pts)
         list_mean_error.append(mean_err)
         list_color.append(camera_color[camera_list[j][0]])
         errors_current_frame.append(mean_err)
 
     list_mean_error_frame.append(np.mean(errors_current_frame))
-    #after each frame include enpty value for display
-    for k in range(0,5):
+    # after each frame include enpty value for display
+    for k in range(0, 5):
         list_mean_error.append(0)
         list_color.append(camera_color[camera_list[0][0]])
 
 
-barlist = plt.bar(range(0,len(list_mean_error_frame)),list_mean_error_frame); 
+barlist = plt.bar(range(0, len(list_mean_error_frame)), list_mean_error_frame)
 plt.xlabel("frame")
 plt.ylabel("Mean reprojection error")
 plt.title("Mean error per frame")
 plt.show()
-'''
+"""
 barlist = plt.bar(range(0,len(list_mean_error)),list_mean_error); 
 for i in range(0,len(list_mean_error)):
     barlist[i].set_color(list_color[i])
 plt.show()
-'''
-
-
-
-
+"""
