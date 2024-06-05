86eb686ad4e99eb297fca31f178cee2f7db2155c
Alex Bailo <alexandr.baylo@gmail.com>
2024-06-05 23:27:19 +0200

    Adding configs and scripts for linters test

--- python_utils/test.sh
+++ python_utils/test.sh
@@ -5,4 +5,4 @@ FILES_TO_FORMAT="compute_error_statistic.py" #"*.py"
 isort $FILES_TO_FORMAT --check-only
 black $FILES_TO_FORMAT --check
 mypy $FILES_TO_FORMAT --disable-error-code=import-untyped
-pylint $FILES_TO_FORMAT --disable=invalid-name
+pylint $FILES_TO_FORMAT
